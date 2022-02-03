4b3e3cf68b8d7cee6bf345f29a334944e29441aa
Mapletree <mapletree@Mapletrees-MacBook-Pro.local>
2022-02-03 23:50:23 +0800

    extracted widget card

--- lib/main.dart
+++ lib/main.dart
@@ -30,36 +30,7 @@ class _QuoteListState extends State<QuoteList> {
   //function that will return a card widget template for our quotes
   //[return type] [function name]
   Widget quoteTemplate(quote) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          //stretch the card from left to right
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-          //text container for Quote Text
-          Text(
-            quote.text,
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.grey[600]
-            ),
-            ),
-          //vertical space using sizedbox
-          const SizedBox(height: 6.0,),
-          //text container for Quote Author
-          Text(
-            quote.author,
-            style: TextStyle(
-              fontSize: 14.0,
-              color: Colors.grey[800]
-            ),
-          )
-          ],
-          ),
-      ),
-    );
+    return QuoteCard(quote: quote); //pass a quote parameter to the QuoteCard class
   }
 
   @override
@@ -82,5 +53,42 @@ class _QuoteListState extends State<QuoteList> {
           //this returns an iterable and list widgets
           children: quotes.map((quote) => quoteTemplate(quote)).toList()),
     );
-  } 
+  }
+}
+
+class QuoteCard extends StatelessWidget {
+  //to accept the parameter, create a constructor
+  final Quote quote;
+
+  QuoteCard({required this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          //stretch the card from left to right
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            //text container for Quote Text
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            //vertical space using sizedbox
+            const SizedBox(
+              height: 6.0,
+            ),
+            //text container for Quote Author
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
